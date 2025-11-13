dac5757fac37057f9f9673ed3972b49579e3fbe4
sudoVijaySoni <100016062+sudoVijaySoni@users.noreply.github.com>
2025-11-13 21:02:37 +0530

    Add console logs for server status

--- server.js
+++ server.js
@@ -41,11 +41,13 @@ app.use("/api/admin", adminRoutes);
 
 // Serve frontend
 app.get("*", (req, res) => {
+    console.log(`Running...`);
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 
 app.get("/api", (req, res) => {
+      console.log(`API Running...`);
   res.json({"message":"success"});
 });
 
